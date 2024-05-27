70da74a96ff4abdb12bb0ec097fba77cba2592ab
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-05-27 20:34:21 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// Create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("red");
+	const [isChangingColors, setIsChangingColors] = useState(false);
+
+	useEffect(() => {
+		let interval;
+
+		if (isChangingColors) {
+			interval = setInterval(() => {
+				setSelectedColor(prevColor => {
+					if (prevColor === "red") return "yellow";
+					if (prevColor === "yellow") return "green";
+					if (prevColor === "green") return "red";
+				});
+			}, 1000);
+		}
+
+		return () => clearInterval(interval);
+	}, [isChangingColors]);
+
+	const ButtonClick = () => {
+		setIsChangingColors(prevState => !prevState);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="top">
+				<div className="traffic-top"></div>
+			</div>
+			<div className="traffic-light">
+			    <div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+				<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+				<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
+			</div>
+			<div className="button">
+				<button onClick={ButtonClick}><strong>{isChangingColors ? "Stop" : "Change Light"}</strong></button>
+			</div>
 		</div>
 	);
 };
